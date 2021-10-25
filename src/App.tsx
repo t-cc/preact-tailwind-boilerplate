f49e456f896f74ee7d65f7295a45b37561180562
Tonio <tonio.castro@gmail.com>
2021-10-25 21:50:58 +0200

    updated editorconfig

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import { h } from 'preact'
+import {h} from 'preact'
 import reactLogo from './assets/logo.svg'
 import './App.css'
 
 export default function App() {
-  return (
-    <div className="App text-center">
-      <header className="flex flex-col">
-        <img src={reactLogo} className=" w-2/3 mx-auto animate-spin" style={{'animationDuration': '10s'}} alt="logo" />
-        <p className='text-white'>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="text-blue-200 hover:text-blue-300"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
+    return (
+        <div className="App text-center">
+            <header className="flex flex-col">
+                <img src={reactLogo} className=" w-2/3 mx-auto animate-spin" style={{'animationDuration': '10s'}}
+                     alt="logo"/>
+                <p className='text-white'>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="text-blue-200 hover:text-blue-300"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  mode: "jit",
-  purge: [
-    './public/*.html',
-    './src/**/*.{ts,tsx}',
-  ],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
+    mode: "jit",
+    purge: [
+        './public/*.html',
+        './src/**/*.{ts,tsx}',
+    ],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {},
+    plugins: [],
 }
